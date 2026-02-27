a531f760746ae381c7090a3f9ba851111872390f
Abdulkarim Mziya <akmziya@Abdulkarims-MacBook-Air-2.local>
2026-02-27 13:05:05 -0800

    chore: initial programmatic UI setup and remove storyboards

---
+++ NewsApp/NewsApp/Controllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsApp
+//
+//  Created by Abdulkarim Mziya on 2026-02-27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemGray2
+    }
+
+
+}
+
---
+++ NewsApp/NewsAppTests/NewsAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAppTests.swift
+//  NewsAppTests
+//
+//  Created by Abdulkarim Mziya on 2026-02-27.
+//
+
+import Testing
+@testable import NewsApp
+
+struct NewsAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
