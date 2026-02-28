819c988119cbc27840733f040eb4bf9ff3db1f59
Abdulkarim Mziya <akmziya@Abdulkarims-MacBook-Air-2.local>
2026-02-27 16:18:16 -0800

    feat: implement news data models and network routing logic

---
+++ NewsApp/NewsApp/Network Services/NetworkHelper.swift
@@ -0,0 +1,8 @@
+//
+//  NetworkHelper.swift
+//  NewsApp
+//
+//  Created by Abdulkarim Mziya on 2026-02-27.
+//
+
+import Foundation
---
+++ NewsApp/NewsApp/Network Services/NewsAPIService.swift
@@ -0,0 +1,8 @@
+//
+//  NewsAPIService.swift
+//  NewsApp
+//
+//  Created by Abdulkarim Mziya on 2026-02-27.
+//
+
+import Foundation
